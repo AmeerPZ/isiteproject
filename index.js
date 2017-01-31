974d32ee5c149d79f83820b96a2d048c697a77c6
AmeerPZ <khana43@national.core.bbc.co.uk>
2017-01-31 18:59:42 +0000

    variable name change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
-var counter = 0;
+var count_number_of_visits = 0;
 
 app.get('/', function (req, res){
-  res.send('Hello World!'+ counter++)
+  res.send('Hello World!'+ count_number_of_visits++)
 })
 
 app.listen(3000, function () {
